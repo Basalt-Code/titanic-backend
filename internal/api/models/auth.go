86eb96b50364f3906d5aaaeea26528e7d1e7a61f
AbdurahmanBorov <abdurahmanborov@mail.ru>
2024-08-26 18:13:22 +0300

    refactoring

--- internal/api/models/auth.go
+++ internal/api/models/auth.go
@@ -5,22 +5,3 @@ type RegisterReq struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,password"`
 }
-
-type LoginReq struct {
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required"`
-}
-
-type TokensResp struct {
-	Access  string `json:"access"`
-	Refresh string `json:"refresh"`
-}
-
-type VerifyEmailReq struct {
-	Email string `json:"email" validate:"required,email"`
-	Code  string `json:"code" validate:"required"`
-}
-
-type RefreshTokenReq struct {
-	RefreshToken string `json:"refreshToken" validate:"required"`
-}
